d3787e53197442d838c91d144f723df29cd0ef9f
Pavel Samoilov <pavel.samoilov@simbirsoft.com>
2022-08-02 23:18:39 +0400

    Реализована логика создания чипсов, парсинга из строки в чипсы, логика события blur

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React, { useState } from 'react';
 import ChipsInput from './components/chips-input/ChipsInput';
 
 const App: React.FC = () => {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>('test,tets');
 
   return (
     <div className="App">
+      <h4>Пример использования готового компонента</h4>
       <ChipsInput value={value} setValue={setValue} />
+      <div>Строковое представление: {value}</div>
     </div>
   );
 }
--- src/components/chips-input/ChipsInput.tsx
+++ src/components/chips-input/ChipsInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss'
 
 interface Props {
@@ -7,13 +7,63 @@ interface Props {
 }
 
 const ChipsInput: React.FC<Props> = ({ setValue, value }) => {
+    const [valueInput, setValueInput] = useState<string>('')
+    const [chips, setChips] = useState<Array<string>>([])
+
+    const [isOpenQuotationMark, setIsOpenQuotationMark] = useState<boolean>(false)
+    const [showErrorMessage, setShowErrorMessage] = useState<boolean>(false)
+
+    const addValueToChips = (chipsString: string) => {
+        setChips([...chips, chipsString])
+        setValueInput('')
+    }
+
     const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value)
+        const { value } = event.target;
+
+        const lastLetter = value[value.length - 1]
+
+        if (lastLetter === '"') {
+            setIsOpenQuotationMark(!isOpenQuotationMark)
+        }
+
+        if (value === ',') return
+
+        if (lastLetter === ',' && !isOpenQuotationMark) {
+            const chipsString = value.substring(0, value.length - 1)
+            addValueToChips(chipsString)
+        } else {
+            setValueInput(value)
+        }
+    }
+
+    useEffect(() => {
+        const str = [...chips, valueInput].join(',')
+        setValue(str)
+    }, [valueInput, chips])
+
+    useEffect(() => {
+        const splitArr = value.split(',')
+        setChips(splitArr)
+    }, [])
+
+    const handleBlur = () => {
+        if (isOpenQuotationMark) {
+            setShowErrorMessage(true)
+        } else {
+            setShowErrorMessage(false)
+            addValueToChips(valueInput)
+        }
     }
 
     return (
         <div className='ChipsInput_wrapper'>
-            <input type="text" className='ChipsInput_input' value={value} onChange={handleChangeInput} />
+            <input type="text"
+                className='ChipsInput_input'
+                value={valueInput}
+                onChange={handleChangeInput}
+                onBlur={handleBlur} />
+            {showErrorMessage && (<p className='ChipsInput_error'>Закройте кавычки с двух сторон</p>)}
         </div>
     )
 }
